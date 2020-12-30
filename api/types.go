f4e1c3bc98e5abace7216e0d9f78746113018e00
Masudur Rahman <masud@appscode.com>
2020-12-30 19:33:49 +0600

    Update License structure
    Signed-off-by: Masudur Rahman <masud@appscode.com>

--- api/types.go
+++ api/types.go
@@ -27,19 +27,19 @@ type LicenseVerificationParams struct {
 
 // License represents the product license for user
 type License struct {
-	Issuer           string           `json:"issuer,omitempty"`
-	Subject          string           `json:"subject,omitempty"`
-	Audience         jwt.Audience     `json:"audience,omitempty"`
-	Expiry           *jwt.NumericDate `json:"expiry,omitempty"`
-	NotBefore        *jwt.NumericDate `json:"not_before,omitempty"`
-	IssuedAt         *jwt.NumericDate `json:"issued_at,omitempty"`
-	ID               string           `json:"id,omitempty"`
-	SubscribedPlans  []string         `json:"subscribed_plans"`
-	SubscriptionID   string           `json:"subscription_id"`
-	SubscriptionName string           `json:"subscription_name"`
-	JWT              string           `json:"jwt"`
-	Status           string           `json:"status"`
-	CanceledAt       *int64           `json:"canceled_at"`
-	IpAddress        *string          `json:"ip_address"`
-	CancelerID       *string          `json:"canceler_id"`
+	Issuer             string           `json:"issuer,omitempty"`
+	Subject            string           `json:"subject,omitempty"`
+	Audience           jwt.Audience     `json:"audience,omitempty"`
+	Expiry             *jwt.NumericDate `json:"expiry,omitempty"`
+	NotBefore          *jwt.NumericDate `json:"not_before,omitempty"`
+	IssuedAt           *jwt.NumericDate `json:"issued_at,omitempty"`
+	ID                 string           `json:"id,omitempty"`
+	SubscribedPlans    []string         `json:"subscribed_plans"`
+	SubscriptionItemID string           `json:"subscription_item_id"`
+	SubscriptionName   string           `json:"subscription_name"`
+	JWT                string           `json:"jwt"`
+	Status             string           `json:"status"`
+	CanceledAt         *int64           `json:"canceled_at"`
+	IpAddress          *string          `json:"ip_address"`
+	CancelerID         *string          `json:"canceler_id"`
 }
